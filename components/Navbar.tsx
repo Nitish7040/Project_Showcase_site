c57dc89d7941a27467446b02f6262e8dc0d046da
Nitish pandey <Nitish7040@users.noreply.github.com>
2025-02-22 10:29:49 +0530

    adding oauth setup for github login

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,52 +1,70 @@
+import { auth, signIn, signOut } from "@/auth";
+
 import Link from "next/link";
 import React from "react";
 
-const Navbar = () => {
+const Navbar = async () => {
+
+const session  = await auth();
+
   return (
     <header>
       <nav className="bg-blue-900 text-white py-4 shadow-md">
         <div className="container mx-auto flex justify-between items-center px-6">
-          {/* Logo */}
           <div className="text-2xl font-bold">
             <h2>Logo</h2>
           </div>
-
-          {/* Navigation Links */}
-          <ul className="flex space-x-6 text-lg font-medium">
-            <li>
-              <Link
-                href="/"
-                className="hover:text-blue-300 transition duration-300"
-              >
-                Home
-              </Link>
-            </li>
-         
+    {session && session?.user ? (
+      <>
+       {/* Navigation Links */}
+       <ul className="flex space-x-6 text-lg font-medium">
             <li>
               <Link
                 href="#"
                 className="hover:text-blue-300 transition duration-300"
               >
-                About
+                create
               </Link>
             </li>
             <li>
-              <Link
-                href="/"
-                className="hover:text-blue-300 transition duration-300"
-              >
-                Logout
-              </Link>
+            <form action={async () => {
+        "use server";
+        await signOut({redirectTo: "/" });
+      }}
+      >
+
+      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Logout
+      </button>
+      </form>
+
+      {/* form for login   */}
             </li>
             <li>
               <Link
-                href="/"
+                href={`/user/sessionid`}  // for redirect to user profile
                 className="hover:text-blue-300 transition duration-300"
               >
-                Username
+               {session?.user?.name}
               </Link>
             </li>
           </ul>
+      
+      </>
+    ) : (
+      <form action={async () => {
+        "use server";
+        await signIn('github');
+      }}
+      >
+
+      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Sign In
+      </button>
+      
+      </form>
+    )}
+         
         </div>
       </nav>
     </header>
